7dc5837ec1f133d1e8e9f36df4adca285fa0bf9c
zaka <zukouch@gmail.com>
2025-09-10 00:22:45 -0700

    .....

--- app/dashboard/achievements/AchievementForm.tsx
+++ app/dashboard/achievements/AchievementForm.tsx
@@ -19,7 +19,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-
 interface AchievementFormProps {
     achievement: Achievement | null;
     onFinished: () => void;
